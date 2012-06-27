baf58e74b92e918ea177a2d3cb81f0c542965903
EC2 Default User <ec2-user@ip-10-100-221-73.ec2.internal>
2012-06-27 03:58:27 +0000

    update

--- software/deployment/config/deploy.rb
+++ software/deployment/config/deploy.rb
@@ -4,11 +4,11 @@ load '/usr/share/tomcat6/scripts/config/aws.config'
 
 sdb = AWS::SimpleDB.new
 
-set :sdb_domain, "automanateestack0"
+set :sdb_domain, "cd06"
 
 set :domain do
-  item = sdb.domains["#{sdb_domain}"].items["parameters"]
-  item.attributes["domain"].values[0].to_s
+  item = sdb.domains["#{sdb_domain}"].items['parameters']
+  item.attributes['ip_address'].values[0].to_s
 end
 
 set :artifact_bucket do
@@ -47,12 +47,12 @@ def from_template(file)
 end
 
 namespace :deploy do
-  
+
   task :setup do
     run "sudo chown -R tomcat:tomcat #{deploy_to}"
     run "sudo service tomcat6 stop"
   end
-  
+
   task :deploy do
     run "cd #{deploy_to} && sudo rm -rf brewery* && sudo wget #{artifact_url}"
   end
@@ -61,11 +61,9 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "sudo service tomcat6 restart"
   end
-  
-  # after "deploy:setup", "deploy:wildtracks_config"
-  # after "deploy:wildtracks_config", "deploy:httpd_conf"
-  # after "deploy:httpd_conf", "deploy:deploy"
-  # after "deploy:httpd_conf", "deploy:deploy"
+
+  after "deploy:setup", "deploy:deploy"
   after "deploy:deploy", "deploy:restart"
 end
+
 
